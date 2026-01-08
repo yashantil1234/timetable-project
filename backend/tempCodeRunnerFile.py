72588f5f13deb95d178399696558d98ff4001bfe
yashantil1234 <yashantil1462@gmail.com>
2026-01-08 17:13:29 +0530

    Save local changes

--- backend/tempCodeRunnerFile.py
+++ backend/tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-# Attach a file (like timetable CSV)
-if attachment_path:
-    with open(attachment_path, "rb") as f:
-        msg.attach(
-            filename=os.path.basename(attachment_path),
-            content_type="text/csv",
-            data=f.read()
-        )
+c
